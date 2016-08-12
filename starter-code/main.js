c8cee65e7d8ebebe5fbc87b85dcd6ade7e0034dc
Winnie <winniecluk@gmail.com>
2016-08-12 07:42:05 -0700

    student delivery

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,13 @@ var cardOne="queen";
 var cardTwo="queen";
 var cardThree="king";
 var cardFour="king";
+
+if (cardTwo===cardFour){
+  alert('Sorry, try again.');
+} else if (cardOne===cardThree){
+  alert('Sorry, try again.');
+} else if (cardOne===cardTwo){
+  alert('You found a match!');
+} else if (cardThree==cardFour){
+  alert('You found a match!')
+}
